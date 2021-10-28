a622089627616d71942b54e5f700b5da194ca1bc
aakashpotdar <potdaraakash@gmail.com>
2021-10-29 00:16:21 +0530

    uc2

---
+++ uc2_employeeWage/uc2_employeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uc2_employeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
