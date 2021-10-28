f52c1f1e24d6017dbd0152047db6bd3fb716d605
aakashpotdar <potdaraakash@gmail.com>
2021-10-28 23:57:30 +0530

    uc1_attendance

---
+++ EmployeePresent_/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeePresent_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            int value1 = ran.Next(3);
+            if (value1 == 1)
+            {
+                Console.WriteLine("the employee is present");
+            }
+            else
+            {
+                Console.WriteLine("the employee is not present");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
