985f4609daff30132f44bbf40308fc0e98deb797
aakashpotdar <potdaraakash@gmail.com>
2021-10-29 11:06:46 +0530

    uc3_partTime

---
+++ uc2_employeeWage/uc3_halfTime/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uc3_halfTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int halfDayWage, partTimeHr = 8,wage=20;
+
+            halfDayWage = partTimeHr * wage;
+
+            Console.WriteLine("Part Time Wages  are " + halfDayWage);
+        }
+    }
+}
