bbbfba3c8776847be741d9fdb7e2b85169c569a7
aakashpotdar <potdaraakash@gmail.com>
2021-10-30 17:03:15 +0530

    uc8_multipleCompaniesDone

---
+++ uc8_multipleCompanies/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uc8_multipleCompanies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            logicClass obj = new logicClass();
+
+            Console.WriteLine("wage of Accenture company" + obj.logicmethod(150));
+            Console.WriteLine("wage of Infosys company" + obj.logicmethod(120));
+            Console.WriteLine("wage of TCS company" + obj.logicmethod(100));
+        }
+    }
+}
