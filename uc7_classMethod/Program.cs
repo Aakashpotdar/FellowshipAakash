5492997b44265bd8c00cd5d4eb08654f74c6de7d
aakashpotdar <potdaraakash@gmail.com>
2021-10-30 14:17:47 +0530

    uc7_method

---
+++ uc7_classMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace uc7_classMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            logicClass obj = new logicClass();
+            obj.logicmethod();
+        }
+    }
+}
---
+++ uc7_classMethod/logicClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uc7_classMethod
+{
+    class logicClass
+    {
+        public void logicmethod() 
+        { 
+            int totalHr = 0, wageOfMonth = 0, workingDays = 1, fullDayWage = 0, halfDayWage = 0, fullDayHr = 20, halfDayHr = 10, wagePerHr = 100;
+            Random ran = new Random();
+
+            while (workingDays <= 20 & totalHr <= 200)
+            {
+                int valueToCheck = ran.Next(3);
+                switch (valueToCheck)
+                {
+                    case 0:
+                        Console.WriteLine("employee is absent on " + workingDays);
+                        workingDays++;
+                        break;
+                    case 1:
+                        fullDayWage = fullDayWage + (fullDayHr* wagePerHr);
+                        workingDays++;
+                        totalHr = totalHr + fullDayHr;
+                        break;
+                    case 2:
+                        halfDayWage = halfDayWage + (halfDayHr* wagePerHr);
+                        workingDays++;
+                        totalHr = totalHr + halfDayHr;
+                        break;
+                }
+            }            
+            wageOfMonth = fullDayWage + halfDayWage;
+            Console.WriteLine("working hours " + totalHr);
+            Console.WriteLine("working days " + workingDays);
+            Console.WriteLine("wages of total month " + wageOfMonth);
+        }
+    }
+}
+
