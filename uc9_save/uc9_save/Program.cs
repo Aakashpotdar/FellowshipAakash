643e3ca7db5f02c2496dd596e2fbeb58ff49cdac
aakashpotdar <potdaraakash@gmail.com>
2021-10-31 20:15:14 +0530

    uc9_done

--- uc9_save/uc9_save/Program.cs
+++ uc9_save/uc9_save/Program.cs
@@ -6,7 +6,18 @@ namespace uc9_save
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            logicClass obj = new logicClass();
+
+            int accentureWage = obj.logicmethod(150,"Accenture");
+            int InfosysWage = obj.logicmethod(120,"Infosys");
+            int TCSWage = obj.logicmethod(100,"TCS");
+
+
+
+            Console.WriteLine("wage of Accenture company " + accentureWage);
+            Console.WriteLine("wage of Infosys company " + InfosysWage);
+            Console.WriteLine("wage of TCS company " + TCSWage);
+
         }
     }
 }
