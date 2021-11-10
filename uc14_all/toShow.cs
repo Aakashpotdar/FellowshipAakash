d04f6f383b4640621140f743b931c9e8e838bae1
aakashpotdar <potdaraakash@gmail.com>
2021-11-10 16:29:53 +0530

    uc14 done

---
+++ uc14_all/toShow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uc14_all
+{
+    interface toShow
+    {
+        public int calculateWage(int wagePerHr, string componyName, int totalWorkingDay, int totalWorkingHour);
+    }
+}
